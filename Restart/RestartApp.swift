b3cddb75fa5fe33d547dfe66e7bc4b989653c5e9
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-06-08 12:00:20 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by tayfun on 8.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
